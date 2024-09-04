95dc7293fe730746d9b140109eb6435e84edd8a2
saravanan2003-hub <saravanasundar506@gmail.com>
2024-09-04 11:36:33 +0530

    switchcase

---
+++ switchOpearator/switch.js
@@ -0,0 +1,28 @@
+let day = 7;
+switch(day)
+{
+    case 1:
+        console.log("this is monday");
+        break;
+    case 2:
+        console.log("this is tuesday");
+        break;
+    case 3:
+        console.log("this is wednesday");
+        break;
+    case 4:
+        console.log("this is wednesday");
+        break;
+    case 5:
+        console.log("this is thursday");
+        break;
+    case 6:
+        console.log("this is friday");
+        break;
+    case 7:
+        console.log("this is saturday");
+        break;
+    default:
+        console.log("invalid day");
+        break;
+}
